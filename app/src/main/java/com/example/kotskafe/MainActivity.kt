e0ebd6dbc434f80fca024d514ef17b1b3b7532e5
ericdecanini <eddecanini@gmail.com>
2023-07-07 15:34:57 +0200

    C3L3

--- app/src/main/java/com/example/kotskafe/MainActivity.kt
+++ app/src/main/java/com/example/kotskafe/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.kotskafe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kotskafe.ui.theme.KotsKafeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +26,7 @@ class MainActivity : ComponentActivity() {
             KotsKafeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    HomeContent()
                 }
             }
         }
@@ -27,17 +34,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeContent() {
+    Column {
+        Image(painter = painterResource(id = R.drawable.coffee), contentDescription = null)
+        Text(modifier = Modifier.padding(16.dp), text = "Coffees", fontSize = 24.sp, fontWeight = FontWeight.Medium)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     KotsKafeTheme {
-        Greeting("Android")
+        HomeContent()
     }
 }
