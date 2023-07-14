c1b8e746298d4d6551dd05243c06854ba62b047c
ericdecanini <eddecanini@gmail.com>
2023-07-14 20:35:25 +0200

    C3L12

--- app/src/main/java/com/example/kotskafe/CartScreen.kt
+++ app/src/main/java/com/example/kotskafe/CartScreen.kt
@@ -1,9 +1,47 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.kotskafe
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CartContent() {
-    Text(text = "Cart")
+    Column {
+        TopAppBar(
+            title = { Text(text = "Cart") },
+            actions = {},
+        )
+        Divider()
+        for (coffee in cart) {
+            CartItem(coffee = coffee)
+        }
+    }
+}
+
+@Composable
+fun CartItem(coffee: Coffee) {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(text = coffee.name, fontWeight = FontWeight.Medium)
+            Text(text = String.format("%.2f", coffee.price))
+        }
+        Divider()
+    }
 }
