48e6208d108bed5a292071a3057633cd9f4412a9
ericdecanini <eddecanini@gmail.com>
2023-07-18 10:31:37 +0200

    C3L15

--- app/src/main/java/com/example/kotskafe/CartScreen.kt
+++ app/src/main/java/com/example/kotskafe/CartScreen.kt
@@ -5,8 +5,10 @@ package com.example.kotskafe
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -15,36 +17,27 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun CartContent(navController: NavController) {
-    val totalPrice = cart.sumOf { it.price }
-    val context = LocalContext.current
-
     Column {
         TopAppBar(
             title = { Text(text = "Cart") },
             actions = {},
         )
         Divider()
-        for (coffee in cart) {
-            CartItem(coffee = coffee)
-        }
-        Text(
-            modifier = Modifier.padding(16.dp),
-            text = "Total: $" + String.format("%.2f", totalPrice),
-            fontWeight = FontWeight.Medium
-        )
-        Button(
-            modifier = Modifier.padding(16.dp),
-            onClick = { onSubmitOrder(navController, context) },
-        ) {
-            Text(text = "Submit Order")
+
+        if (cart.isNotEmpty()) {
+            CartList(navController = navController)
+        } else {
+            EmptyCart()
         }
     }
 }
@@ -68,5 +61,38 @@ fun CartItem(coffee: Coffee) {
             Text(text = String.format("%.2f", coffee.price))
         }
         Divider()
+    }
+}
+
+@Composable
+fun EmptyCart() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "Your cart is empty",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium,
+        )
+    }
+}
+
+@Composable
+fun CartList(navController: NavController) {
+    val totalPrice = cart.sumOf { it.price }
+    val context = LocalContext.current
+
+    for (coffee in cart) {
+        CartItem(coffee = coffee)
+    }
+    Text(
+        modifier = Modifier.padding(16.dp),
+        text = "Total: $" + String.format("%.2f", totalPrice),
+        fontWeight = FontWeight.Medium
+    )
+    Button(
+        modifier = Modifier.padding(16.dp),
+        onClick = { onSubmitOrder(navController, context) },
+    ) {
+        Text(text = "Submit Order")
     }
 }
